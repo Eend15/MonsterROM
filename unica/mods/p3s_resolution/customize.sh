0f710d8260f4b78982930d926f0a5b8638aae414
root <root@DESKTOP-T08PTUL.localdomain>
2026-08-24 13:10:32 +0200

    p3s: make resolution patch idempotent

--- unica/mods/p3s_resolution/customize.sh
+++ unica/mods/p3s_resolution/customize.sh
@@ -17,7 +17,22 @@ if grep -q -F '"ro.product.device"' "$DISPLAY_UTILS"; then
     EVAL "sed -i 's/\"ro.product.device\"/\"ro.product.vendor.device\"/g' \"$DISPLAY_UTILS\""
 fi
 
-SMALI_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
-    "${DISPLAY_UTILS#"$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk/"}" "return" \
-    "canSetHighRefreshRateAboveWQHD(Landroid/content/Context;)Z" \
-    "true"
+if ! awk '
+    /^\.method .*canSetHighRefreshRateAboveWQHD\(Landroid\/content\/Context;\)Z/ {
+        in_method = 1
+    }
+    in_method && /const\/4 p0, 0x1/ {
+        patched = 1
+    }
+    in_method && /^\.end method/ {
+        in_method = 0
+    }
+    END {
+        exit patched ? 0 : 1
+    }
+' "$DISPLAY_UTILS"; then
+    SMALI_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
+        "${DISPLAY_UTILS#"$APKTOOL_DIR/system/priv-app/SecSettings/SecSettings.apk/"}" "return" \
+        "canSetHighRefreshRateAboveWQHD(Landroid/content/Context;)Z" \
+        "true"
+fi
